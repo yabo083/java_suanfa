0d53f6ac55aa3cc1c93bb191a457640b65bc9a63
Yabo <2657455842@qq.com>
2023-04-02 12:13:19 +0800

    计算组合数

---
+++ src/com/tools/Combination.java
@@ -0,0 +1,25 @@
+package com.tools;
+
+public class Combination {
+
+
+    /**
+     * 计算组合数
+     * @param a 是n
+     * @param b 是m
+     * @return 返回结果
+     */
+    public static long C(long a, long b) {
+        long res = 1;
+        for (long i = a, j = 1; j <= b; i--, j++) {
+            res = res * i / j;
+//            if (res > n) return res;//特殊时候有用
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(C(5, 2));
+    }
+}
