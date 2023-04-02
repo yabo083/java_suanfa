fe9f6b20ae8e056322459c0d4df931f4500f7fd9
Yabo <2657455842@qq.com>
2023-04-02 18:23:03 +0800

    Read类大升级！

--- src/com/tools/ReadAndWrite.java
+++ src/com/tools/ReadAndWrite.java
@@ -1,37 +1,59 @@
 package com.tools;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 class ReadAndWrite {
-    public static class Read {
+    static class Read {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));//快写
 
-    StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-    PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));//快写
+        Read() throws IOException {
+        }
+        //read类升级，由StreamTokenizer变成StringTokenizer
+        String next() {
+            while (st==null||!st.hasMoreElements()){
+                try {
+                    st=new StringTokenizer(br.readLine());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
 
-    //快读一个整数
-    public int nextInt() throws IOException {
-        st.nextToken();
-        return (int) st.nval;
+        //快读一个整数
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        //这下换成br，就可以读任意类型的字符串了
+        public String readLine() throws IOException {
+            return br.readLine();
+        }
+
+        //一个快写任何类型的方法,报错改成泛型
+        public <T> void write(T o) {
+            pw.println(o);
+            pw.flush();
+        }
+
+
     }
 
-    public long nextLong() throws IOException {
-        st.nextToken();
-        return (long) st.nval;
+    public static void main(String[] args) throws IOException {
+        Read sc = new Read();
+        //生成read所有方法的测试用例
+
+
     }
-
-    //只能读取纯字符串的方法
-    public String readLine() throws IOException {    //	不推荐使用
-        st.nextToken();
-        return st.sval;
-    }
-
-    //一个快写任何类型的方法,报错改成泛型
-    public void write(Object o) {
-        pw.println(o);
-        pw.flush();
-    }
-
-
-}
 }
 
