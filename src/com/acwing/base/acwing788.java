96b99ab6f72dee3f18a65eb119b16b5ab225ad54
yabo083 <103875886+yabo083@users.noreply.github.com>
2023-08-29 20:28:20 +0800

    更新每日一题

---
+++ src/com/acwing/base/acwing788.java
@@ -0,0 +1,65 @@
+package com.acwing.base;
+
+import java.io.*;
+
+public class acwing788 {
+
+    public static int N = (int) (1e5 + 10);
+
+    public static int[] a = new int[N], tmp = new int[N];
+
+    public static void main(String[] args) throws IOException {
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.write(merge_sort(a, 0, n - 1));
+        sc.pw.flush();
+    }
+
+    private static long merge_sort(int[] q, int l, int r) {
+        if (l >= r) return 0;
+
+        int mid = l + r >> 1;
+
+        long  res = merge_sort(q, l, mid) + merge_sort(q, mid + 1, r);
+
+        int k = 0, i = l, j = mid + 1;
+
+        while (i <= mid && j <= r){
+            if (q[i] <= q[j])
+                tmp[k ++] = q[i ++];
+            else{
+                res += mid - i + 1;
+                tmp[k ++] = q[j ++];
+            }
+        }
+
+        while (i <= mid ) tmp[k ++ ] = q[i ++];
+        while (j <= r) tmp[k ++] = q[j ++];
+
+        for ( i = l , j = 0; i <= r; i ++, j ++)
+            q[i] = tmp[j];
+
+        return res;
+    }
+
+
+    public static class sc {
+
+        static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+        public static int nextInt() throws IOException {
+            st.nextToken();
+            return (int) st.nval;
+        }
+
+        public static <T> void write(T o) {
+            pw.print(o);
+        }
+
+
+    }
+
+}
--- src/com/acwing/base/acwing789.java
+++ src/com/acwing/base/acwing789.java
@@ -8,73 +8,53 @@ public class acwing789 {
     static int n, m;
     static int[] q = new int[N];
 
-    public static class Read {
+    public static void main(String[] args) throws IOException {
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            q[i] = sc.nextInt();
+        }
 
-        StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));//快写
+        while (m -- > 0){
+            int x = sc.nextInt();
 
-        //快读一个整数
-        public int nextInt() throws IOException {
+            int l = 0, r = n - 1;
+            while (l < r){
+                int mid = l + r >> 1;
+                if (q[mid] >= x) r = mid;
+                else l = mid + 1;
+            }
+            if (q[l] != x ) sc.write("-1 -1");
+            else {
+                sc.write(l);
+                l = 0;
+                r = n - 1;
+                while (l < r){
+                    int mid = l + r + 1 >> 1;
+                    if (q[mid] <= x)  l = mid;
+                    else r = mid - 1;
+                }
+                sc.write(l);
+            }
+        }
+
+        sc.pw.flush();
+    }
+
+    public static class sc {
+
+        static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+        public static int nextInt() throws IOException {
             st.nextToken();
             return (int) st.nval;
         }
 
-        public long nextLong() throws IOException {
-            st.nextToken();
-            return (long) st.nval;
-        }
-
-        //只能读取纯字符串的方法
-        public String readLine() throws IOException {    //	不推荐使用
-            st.nextToken();
-            return st.sval;
-        }
-
-        //一个快写任何类型的方法,报错改成泛型
-        public void write(Object o) {
+        public static <T> void write(T o) {
             pw.print(o);
-            pw.flush();
         }
-    }
 
 
-    public static void main(String[] args) throws IOException {
-        Read sc = new Read();
-        n = sc.nextInt();
-        m = sc.nextInt();
-        for (int i = 0; i < n; i ++)
-            q[i] = sc.nextInt();
-
-        while (m -- > 0){
-            int x = sc.nextInt();
-            int l = -1,r = n;
-            while(l+1!=r)//当l与r没有相接的时候,求边界
-            {
-                int mid=l + (r - l) /2;
-                //下面找第一个>=5的坐标
-                if(q[mid]>=x)
-                    r=mid;
-                else
-                    l=mid;
-            }
-            if (q[r] != x) {
-                sc.write("-1 -1");
-                sc.pw.println();
-            } else {
-                sc.write(r + " ");
-                l = -1;
-                r = n ;
-                while (l+1!=r){
-                    int mid = l + (r - l) / 2;
-                    if (q[mid] <= x)
-                        l = mid;
-                    else
-                        r = mid ;
-                }
-                if (q[l] != x) sc.write(r);
-                else sc.write(l);
-                sc.pw.println();
-            }
-        }
     }
 }
