cc33d2ce5933c7fd230d309713d4718d39d258e4
yabo083 <103875886+yabo083@users.noreply.github.com>
2023-10-20 15:57:30 +0800

    更新每日一题

--- src/com/acwing/base/acwing2.java
+++ src/com/acwing/base/acwing2.java
@@ -1,52 +1,50 @@
 package com.acwing.base;
 
 import java.io.*;
-import java.util.Optional;
 
 public class acwing2 {
-    static int N = 1010;
 
-    static int n, m;
-    static int[] v = new int[N], w = new int[N];
-    static int[][] f = new int[N][N];
+    public static int N = 1010;
+
+    public static int n, m;
+    public static int[] v = new int[N], w = new int[N];
+    public static int[] f = new int[N];
 
     public static void main(String[] args) throws Exception {
-        Read bf = new Read();
-//        Scanner bf = new Scanner(System.in);
-        n = bf.nextInt();
-        m = bf.nextInt();
-        for (int i = 1; i <= n; i ++) {
-            v[i] = bf.nextInt();
-            w[i] = bf.nextInt();
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            v[i] = sc.nextInt();
+            w[i] = sc.nextInt();
         }
 
-        for (int i = 1; i <= n; i ++){
-            for (int j = 0; j <= m; j ++ ){
-                f[i][j] = f[i - 1][j];
-                if (j >= v[i]) f[i][j] = Math.max(f[i][j], f[i-1][j - v[i]] + w[i]);
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= v[i]; j--) {
+                f[j] = Math.max(f[j], f[j - v[i]] + w[i]);
             }
         }
 
-//        System.out.println(f[n][m]);
-        bf.write(f[n][m]);
-        bf.pw.flush();
+        sc.write(f[m]);
+        sc.pw.flush();
 
 
     }
-    static class Read{
 
-        StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));//快写
+    public static class sc {
 
-        //快读一个整数
-        public int nextInt() throws Exception{
+        static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+        public static int nextInt() throws IOException {
             st.nextToken();
             return (int) st.nval;
         }
-        //写一个快写任何类型的方法
-        public void write(Object o) throws Exception{
-            pw.println(o);
+
+        public static <T> void write(T o) {
+            pw.print(o);
         }
 
+
     }
+
 }
