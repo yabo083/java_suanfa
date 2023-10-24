ea0692fe8920b720f17277b91fff2c57495c60a7
yabo083 <103875886+yabo083@users.noreply.github.com>
2023-10-24 18:10:35 +0800

    更新每日一题

---
+++ src/com/acwing/base/acwing3.java
@@ -0,0 +1,47 @@
+package com.acwing.base;
+
+import java.io.*;
+
+public class acwing3 {
+
+    public static int N = 1010;
+    public static int n, m;
+    public static int[] v = new int[N], w = new int[N];
+    public static int[] f = new int[N];
+
+    public static void main(String[] args) throws IOException {
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            v[i] = sc.nextInt();
+            w[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = v[i]; j <= m; j++) {
+                f[j] = Math.max(f[j], f[j - v[i]] + w[i]);
+            }
+        }
+
+        sc.write(f[m]);
+        sc.pw.flush();
+    }
+
+    public static class sc {
+
+        static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+        public static int nextInt() throws IOException {
+            st.nextToken();
+            return (int) st.nval;
+        }
+
+        public static <T> void write(T o) {
+            pw.print(o);
+        }
+
+
+    }
+}
