78d9a80a17b96f5b529d400f7b23b73d2ad84b56
yabo083 <103875886+yabo083@users.noreply.github.com>
2023-10-25 20:31:22 +0800

    多重背包，朴素做法，基于01背包再加一层循环

--- src/com/acwing/base/acwing4.java
+++ src/com/acwing/base/acwing4.java
@@ -1,7 +1,6 @@
 package com.acwing.base;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class acwing4 {
 
@@ -9,7 +8,6 @@ public class acwing4 {
     public static int n, m;
     public static int[] v = new int[N], w = new int[N], s = new int[N];
     public static int[] f = new int[N];
-
 
     public static void main(String[] args) throws IOException {
         n = sc.nextInt();
@@ -21,26 +19,11 @@ public class acwing4 {
             s[i] = sc.nextInt();
         }
 
-        int sum = Arrays.stream(s).sum();
-        int[] ww = new int[sum + 1];
-        int[] vv = new int[sum + 1];
-
-        // 下面是拆成一个一个的包的代码
-        int num = 1;
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < s[i]; j++) {
-                vv[num] = v[i];
-                ww[num++] = w[i];
-            }
-        }
-
-//        for (int i = 1; i < num; i ++){
-//            sc.write("体积：" + vv[i] + " 价值：" + ww[i] + "\n");
-//        }
-
-        for (int i = 1; i < num; i++) {
-            for (int j = m; j >= vv[i]; j--) {
-                f[j] = Math.max(f[j], f[j - vv[i]] + ww[i]);
+            for (int j = m; j >= v[i]; j--) {
+                for (int k = 0; k <= s[i] && k * v[i] <= j; k++) {
+                    f[j] = Math.max(f[j], f[j - v[i] * k] + w[i] * k);
+                }
             }
         }
         sc.write(f[m]);
